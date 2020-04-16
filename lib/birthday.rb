24f19f667a5a4c44f45b4124afeba0d258bfdeb2
William Mittard <billymittard@ymail.com>
2020-04-16 18:40:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,14 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 
-
+def age_appropriate_birthday(birthday_kids)
+birthday_kids.each do |kids_name, age|
+if age < 12 
+puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+esle
+"There is a sentence in spec.ruby"
+end
+end
 
 
 
