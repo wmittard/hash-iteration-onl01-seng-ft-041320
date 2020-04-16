d511d9fd79ec10a2819d757230e5b5a03fc0ee07
William Mittard <billymittard@ymail.com>
2020-04-16 19:04:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,8 +13,8 @@ def age_appropriate_birthday(birthday_kids)
 birthday_kids.each do |kids_name, age|
 if age < 12 
 puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-end 
 else puts "You are now too old for this" 
+end
 end 
 end 
 
