1fc844868ec708aac9385ce33b503dd482fd542c
William Mittard <billymittard@ymail.com>
2020-04-16 18:34:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,7 +11,7 @@ def happy_birthday(birthday_kids)
 
 def age_appropriate_birthday(birthday_kids)
 birthday_kids.each do |kids_name, age| 
-    p "Happy Birthday#{kids_name}! You are now #{age} years old!"
+    puts "Happy Birthday#{kids_name}! You are now #{age} years old!"
   end
 end
 
